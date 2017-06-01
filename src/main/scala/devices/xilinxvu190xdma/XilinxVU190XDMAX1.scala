eb658ddd3cac8ea17bb72148d5e990aeb0e36e76
Austin Harris <austinharris@utexas.edu>
2017-05-31 19:51:02 -0500

    Update XDMA as per updated vc707mig (axi4 pipelined converters and buffer in front of ddr controller)

--- src/main/scala/devices/xilinxvu190xdma/XilinxVU190XDMAX1.scala
+++ src/main/scala/devices/xilinxvu190xdma/XilinxVU190XDMAX1.scala
@@ -29,16 +29,23 @@ class XilinxVU190XDMA(implicit p: Parameters) extends LazyModule {
                                                          regionType    = RegionType.UNCACHED,
                                                          executable    = true,
                                                          supportsWrite = TransferSizes(1, 256*8),
-                                                         supportsRead  = TransferSizes(1, 256*8),
-                                                         interleavedId = Some(0))),
+                                                         supportsRead  = TransferSizes(1, 256*8))),
                                           beatBytes = 8)))
 
   //val xing = LazyModule(new TLAsyncCrossing)
-  val toaxi4 = LazyModule(new TLToAXI4(idBits = 4))
+  val toaxi4  = LazyModule(new TLToAXI4(beatBytes = 8))
+  val indexer = LazyModule(new AXI4IdIndexer(idBits = 4))
+  val deint   = LazyModule(new AXI4Deinterleaver(p(coreplex.CacheBlockBytes)))
+  val yank    = LazyModule(new AXI4UserYanker)
+  val buffer  = LazyModule(new AXI4Buffer)
 
-  //xing.node := node
+  // xing.node := node
   val monitor = (toaxi4.node := node)
-  axi4 := toaxi4.node
+  axi4 := buffer.node
+  buffer.node := yank.node
+  yank.node := deint.node
+  deint.node := indexer.node
+  indexer.node := toaxi4.node
 
    lazy val module = new LazyModuleImp(this) {
     val io = new Bundle {
@@ -55,16 +62,6 @@ class XilinxVU190XDMA(implicit p: Parameters) extends LazyModule {
     ibufds_gte3.io.CEB := UInt(0)
     ibufds_gte3.io.I := io.port.pcie_sys_clk_clk_p
     ibufds_gte3.io.IB := io.port.pcie_sys_clk_clk_n
-
-    //Generate 50MHz clock from the 100MHz pcie ref clk
-    // val bufg_gt = Module(new BUFG_GT)
-    // bufg_gt.io.CE := Bool(true)
-    // bufg_gt.io.CEMASK := Bool(false)
-    // bufg_gt.io.CLRMASK := Bool(false)
-    // bufg_gt.io.DIV := UInt(1)
-    // bufg_gt.io.I := ibufds_gte3.io.ODIV2
-    // bufg_gt.io.CLR := Bool(false)
-    // io.port.sys_clk_50 := bufg_gt.io.O
 
     //pins to top level
     io.port.c0_init_calib_complete := blackbox.io.c0_init_calib_complete
@@ -105,11 +102,10 @@ class XilinxVU190XDMA(implicit p: Parameters) extends LazyModule {
     toaxi4.module.clock := blackbox.io.s01_aclk
     // toaxi4.module.reset := blackbox.io.s01_aresetn
     toaxi4.module.reset := io.port.safe_aresetn 
-     monitor.foreach { lm =>
-       lm.module.clock := blackbox.io.s01_aclk
-       // lm.module.reset := blackbox.io.s01_aresetn
-       lm.module.reset := blackbox.io.s01_aresetn
-     }
+    (Seq(toaxi4, indexer, deint, yank, buffer) ++ monitor) foreach { lm =>
+      lm.module.clock := blackbox.io.s01_aclk
+      lm.module.reset := blackbox.io.s01_aresetn
+    }
     //xing.module.io.out_clock := blackbox.io.c0_ddr4_ui_clk
     //xing.module.io.out_reset := blackbox.io.c0_ddr4_ui_clk_sync_rst
     //toaxi4.module.clock := blackbox.io.c0_ddr4_ui_clk
