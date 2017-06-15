434dd38bed5ca8597b363fb8fa1c5f88931f79c6
Austin Harris <austinharris@utexas.edu>
2017-06-14 21:58:01 -0500

    Update to vu190 dma for multi-io module changes

--- src/main/scala/devices/xilinxvu190xdma/XilinxVU190XDMAPeriphery.scala
+++ src/main/scala/devices/xilinxvu190xdma/XilinxVU190XDMAPeriphery.scala
@@ -2,29 +2,27 @@ package sifive.blocks.devices.xilinxvu190xdma
 
 import Chisel._
 
-import diplomacy._
-import rocketchip.{
-  HasTopLevelNetworks,
-  HasTopLevelNetworksModule,
-  HasTopLevelNetworksBundle
-}
-import coreplex.BankedL2Config
+import diplomacy.{LazyModule, LazyMultiIOModuleImp}
+import rocketchip.HasSystemNetworks
 
-trait HasPeripheryXilinxVU190XDMA extends HasTopLevelNetworks {
-  val module: HasPeripheryXilinxVU190XDMAModule
+trait HasPeripheryXilinxVU190XDMA extends HasSystemNetworks {
+  val module: HasPeripheryXilinxVU190XDMAModuleImp
 
   val xilinxvu190xdma = LazyModule(new XilinxVU190XDMA)
-  require(p(BankedL2Config).nMemoryChannels == 1, "Coreplex must have 1 master memory port")
+  require(nMemoryChannels == 1, "Coreplex must have 1 master memory port")
   xilinxvu190xdma.node := mem(0).node
 }
 
-trait HasPeripheryXilinxVU190XDMABundle extends HasTopLevelNetworksBundle {
-  val xilinxvu190xdma = new XilinxVU190XDMAIO
+trait HasPeripheryXilinxVU190XDMABundle {
+  val xilinxvu190xdma: XilinxVU190XDMAIO
+  def connectXilinxVU190XDMAToPads(pads: XilinxVU190XDMAPads) {
+    pads <> xilinxvu190xdma
+  }
 }
 
-trait HasPeripheryXilinxVU190XDMAModule extends HasTopLevelNetworksModule {
+trait HasPeripheryXilinxVU190XDMAModuleImp extends LazyMultiIOModuleImp {
   val outer: HasPeripheryXilinxVU190XDMA
-  val io: HasPeripheryXilinxVU190XDMABundle
+  val xilinxvu190xdma = IO(new XilinxVU190XDMAIO)
 
-  io.xilinxvu190xdma <> outer.xilinxvu190xdma.module.io.port
+  xilinxvu190xdma <> outer.xilinxvu190xdma.module.io.port
 }
